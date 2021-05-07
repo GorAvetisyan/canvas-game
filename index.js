5c897fa3bcb327d5a382a8d690c299140c31259c
Zero462486 <gor03avetisyan@gmail.com>
2021-05-07 16:37:34 +0400

    Added Game class

--- index.js
+++ index.js
@@ -20,37 +20,38 @@ const boosterTypes = [
 ]
 
 
-document.addEventListener('keydown', ({key, type}) => game.player.handleKeyInput(key, type));
-document.addEventListener('keyup', ({key, type}) => game.player.handleKeyInput(key, type));
 
-const game = {
-  bulletsArray : [],
-  monstersArray : [],
-  boostersArray : [],
-  boosterTime : 3,
-  boosterSize : 30,
-  lastBoosterTime : -1,
-  boosterLiveTime : 10,
-  maxMonsterCount : 10,
-  monsterMaxRoad : 1000,
-  score : 0,
-  bulletSize : 10,
-  player : new Player(0, 0, 10, 50, 100, 0.1, 5, 20, 20, 3),
-  bullets : [],
-  status : 'menu',
 
-  start: () => {
+class Game{
+  constructor(boosterTime, boosterSize, boosterLiveTime, maxMonsterCount, monsterMaxRoad, bulletSize, player){
+    this.boosterLiveTime = boosterLiveTime;
+    this.maxMonsterCount = maxMonsterCount;
+    this.monsterMaxRoad = monsterMaxRoad;
+    this.boosterTime = boosterTime;
+    this.boosterSize = boosterSize;
+    this.player = player;
+    this.bulletSize = bulletSize;
+    this.bulletsArray = [];
+    this.monstersArray = [];
+    this.boostersArray = [];
+    this.lastBoosterTime = -1;
+    this.score = 0;
+    this.status = 'menu';
+  }
+  
+  start = () => {
     // game.boostersArray.push(new LifeBooster(200, 200, 100, 'red', 1, new Date().valueOf()));    
-    game.status = 'playing';
-    game.animate();
-  },
+    this.status = 'playing';
+    this.animate();
+  }
 
-  animate: () => {
-    if(game.status === 'playing'){
+  animate = () => {
+    console.log('Game');
+    if(this.status === 'playing'){
       ctx.clearRect(0, 0, innerWidth, innerHeight);
-      requestAnimationFrame(game.animate);
+      requestAnimationFrame(this.animate);
       
-      if(game.lastBoosterTime === -1 || (new Date().valueOf() - game.lastBoosterTime) / 1000 >= game.boosterTime ){
+      if(this.lastBoosterTime === -1 || (new Date().valueOf() - this.lastBoosterTime) / 1000 >= this.boosterTime ){
         const x = randomNumber(0, innerWidth);
         const y = randomNumber(0, innerHeight);
         const typeIndex = Math.round(randomNumber(0, boosterTypes.length - 1));
@@ -58,78 +59,80 @@ const game = {
         let booster;
         switch (type){
           case 'life':
-            booster = new LifeBooster(x, y, game.boosterSize, color, new Date().valueOf(), type);
+            booster = new LifeBooster(x, y, this.boosterSize, color, new Date().valueOf(), type);
             break;
           case 'speed':
-            booster = new SpeedBooster(x, y, game.boosterSize, color, new Date().valueOf(), type, 5, 2);
+            booster = new SpeedBooster(x, y, this.boosterSize, color, new Date().valueOf(), type, 5, 2);
             break;
           case 'super-shot':
-            booster = new SuperShotBooster(x, y, game.boosterSize, color, new Date().valueOf(), type, 5, 100);
+            booster = new SuperShotBooster(x, y, this.boosterSize, color, new Date().valueOf(), type, 5, 100);
             break;
         }
-        game.lastBoosterTime = new Date().valueOf();
-        game.boostersArray.push(booster);
+        this.lastBoosterTime = new Date().valueOf();
+        this.boostersArray.push(booster);
       }
-  
-      game.boostersArray.forEach(item => {
-        if((new Date().valueOf() - item.timestamp) / 1000 >= game.boosterLiveTime){
+      this.boostersArray.forEach(item => {
+        if((new Date().valueOf() - item.timestamp) / 1000 >= this.boosterLiveTime){
           item.delete();
         }
-        if(isCollision(item, game.player)){
+        if(isCollision(item, this.player)){
           item.eatenByUser();
         }
         item.draw();
       });
-  
-      game.bulletsArray.forEach(item => item.update());
+      this.bulletsArray.forEach(item => item.update());
       
       
-      game.player.draw();
-      game.player.update();
+      this.player.draw();
+      this.player.update();
   
-      for (let i = game.monstersArray.length; i < game.maxMonsterCount; i++) {
+      for (let i = this.monstersArray.length; i < this.maxMonsterCount; i++) {
         let x = randomNumber(0, innerWidth);
         let y = randomNumber(0, innerHeight);
-        for (let j = 0; j < game.monstersArray.length; j++) {
-          if(game.monstersArray[j].isCollision(game.player)){
+        for (let j = 0; j < this.monstersArray.length; j++) {
+          if(this.monstersArray[j].isCollision(this.player)){
             console.log('Is collision');
             x = randomNumber(0, innerWidth);
             y = randomNumber(0, innerHeight);
-            game.monstersArray[j].x = x;
-            game.monstersArray[j].x = y;
+            this.monstersArray[j].x = x;
+            this.monstersArray[j].x = y;
             j = -1;
           }
           
         }
         const dx = randomNumber(-3, 3);
         const dy = randomNumber(-3, 3);
-        const maxRoad = randomNumber(0, game.monsterMaxRoad);
+        const maxRoad = randomNumber(0, this.monsterMaxRoad);
         const monster = new Monster(x, y, 50, dx, dy, maxRoad);
-        game.monstersArray.push(monster);
+        this.monstersArray.push(monster);
         
       }
   
   
   
-      game.monstersArray.forEach(item => {
-        if(item.isCollision(game.player)){
-          game.player.minusLive();
+      this.monstersArray.forEach(item => {
+        if(item.isCollision(this.player)){
+          this.player.minusLive();
           item.delete();
         }
         item.update();
         item.checkForCollisionWithBullet();
       })
     }
-  },
-    
-
-  over : () => {
-    game.status = 'result';
-    alert('Game over');
+  }
+  
+  over = () => {
+    this.status = 'result';
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
   }
 
 }
   
+const player = new Player(0, 0, 10, 50, 100, 0.1, 5, 20, 20, 3);
+const game = new Game(10, 30, 3, 10, 1000, 10, player);
+
+document.addEventListener('keydown', ({key, type}) => game.player.handleKeyInput(key, type));
+document.addEventListener('keyup', ({key, type}) => game.player.handleKeyInput(key, type));
 
 game.start();
 
